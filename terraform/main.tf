99e266598abe7805be5c7af06554fba30ce92478
Saurabh <chanchlesh2597@gmail.com>
2026-04-01 13:16:48 +0530

    Change AWS region and add S3 bucket resource
    Updated AWS region and added S3 bucket resource with random suffix.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,11 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "devops-demo-server"
-  }
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
-resource "aws_security_group" "web_sg" {
-  name = "web-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "vault-demo-bucket-${random_id.bucket_suffix.hex}"
 }
